d59485972b370d04d97d5a8149d39701518265fe
Isabelli Batista <isabellibatista7@gmail.com>
2020-03-03 18:27:33 -0300

    parte 1

---
+++ js/main.js
@@ -0,0 +1,58 @@
+//var é para variavel
+
+// var nome= "isabelli";
+// var idade= 19;
+// var idade2=12;
+// var time="japao é top"
+
+
+//alert é o modal que aparece 
+
+// //alert(nome + " tem " + idade + " anos");
+// //alert(idade+idade2);
+
+
+//console.log é para aparecer no console do inspecionar
+
+// console.log(nome);
+// console.log(idade+idade2);
+
+//toUpperCase é para deixar em caixa alta e toLowerCase é caixa normal
+// console.log(time.toUpperCase());
+
+//array
+var lista = ["maca", "pera", "uva", "banana"];
+
+//push adiciona um elemento no array
+//lista.push("uva");
+
+//pop apaga o ultimo elemento do array
+lista.pop();
+
+//mostra o tamanho do array
+console.log(lista.length);
+
+var palavra="abcde";
+console.log(palavra.length);
+
+//mostra a lista ao contrario
+//console.log(lista.reverse());
+
+//transforma todo mundo em string
+// console.log(lista);
+// console.log(lista.toString());
+
+//mostra só o item 1 da lista
+console.log(lista[1]);
+//mostra a segunda letra da string
+console.log(lista.toString()[1]);
+
+//troca a virgula por - ou o que colocar nos parenteses
+console.log(lista.join(" - "));
+
+//especificando os array
+var frutas=[{nome:"maca", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+console.log(frutas[0]);
+
+
